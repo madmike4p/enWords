74675df63003fc6c17e50be13abbd50e86179cf7
madmike4p <madmike.4p@gmail.com>
2019-10-17 04:47:11 +0200

    poczatki szukania slowa

--- platforms/browser/www/js/page_dict.js
+++ platforms/browser/www/js/page_dict.js
@@ -1,3 +1,41 @@
+function errorDB(err) { alert("Error processing SQL: "+ err.code + " " + err.message); }
+function successDB() {}
+
+function searchWordLike(param) {
+  return function (tx) {
+      tx.executeSql("select id, gb_word from words where gb_word like ?", [param], function(tx1, result) {	 
+        
+        var tab = [];
+        for (var x = 0; x < result.rows.length; x++) {
+          var line = '<a href="#" data-id="' + result.rows.item(x).id + '" class="enWord">' + result.rows.item(x).gb_word + '</a><br>';
+          tab.push(line);
+        }
+        
+        if (tab.length > 0) {
+          document.getElementById("wordsEN").innerHTML = tab.join(" ");
+        }
+      }, errorDB);  
+      
+      tx.executeSql("select id, pl_word from words where pl_word like ?", [param], function(tx1, result) {	 
+        
+        var tab = [];
+        for (var x = 0; x < result.rows.length; x++) {
+          var line = '<a href="#" data-id="' + result.rows.item(x).id + '" class="enWord">' + result.rows.item(x).pl_word + '</a><br>';
+          tab.push(line);
+        }
+        
+        if (tab.length > 0) {
+          document.getElementById("wordsPL").innerHTML = tab.join(" ");
+        }
+      }, errorDB);  
+      
+      app.bindWords();
+  }
+}
+
+
+var db = window.openDatabase("enWords.db", "1.0", "enWords", 1000000);
+
 var app = {
 
     initialize: function() {
@@ -10,8 +48,27 @@ var app = {
     
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        
+        $(".enWord").click(function() {
+          alert("enWord");
+        });
+        
+        $("#search-1").on("input", function(){
+          var searchString = $(this).val() + '%';
+          if ($(this).val().length > 3) db.transaction(searchWordLike(searchString), errorDB, successDB);
+        });
+        
+        
+        
     }, // onDeviceReady
 
+    bindWords: function() {
+        $(".enWord").off();
+        $(".enWord").click(function() {
+          alert("...enWord " + $(this).attr("data-id"));
+        });
+    },
+    
     receivedEvent: function(id) {
     } // receivedEvent
 };
