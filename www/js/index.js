13bed72ecd8269668a8c2595cb176607cf811c0c
mike <mike@localhost>
2019-10-25 11:48:11 +0200

    changes

--- www/js/index.js
+++ www/js/index.js
@@ -13,7 +13,30 @@ var app = {
 
   onClick: function(event) {
     var container = document.getElementById('container');
-    $.get('https://google.com', function(data) {alert(data);});
+    var button = document.getElementById('button');
+    button.innerHTML = 'Pushed';
+    alert('pushed');
+
+
+  var xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function() {
+
+    if (this.readyState == 4 && this.status == 200) {
+
+      alert(this.status);
+
+      alert(this.statusText);
+
+      alert(this.responseText);
+
+    }
+
+  };
+
+  xhttp.open("GET", 'https://google.com/', true);
+
+  xhttp.send();
   },
 
   dbMessage: function(msg) {
